88f0a5cec53df4ae750af15f60f025d11f91d5a8
Zrossiz <soon.pluxury@mail.ru>
2024-01-23 22:50:55 +0300

    feat: added mainBg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
 import { withLayout } from "@/layout/Layout";
+import styles from '../styles/Home.module.scss';
+import Image from "next/image";
 
 
 const Home = () => {
   return (
-    <></>
+    <>
+      <section className={styles.firstDisplay}>
+        <div className={styles.backgroundImgWrapper}>
+          <div className={styles.backgroundImg}></div>
+        </div>
+        <div>title</div>
+      </section>
+    </>
   );
 }
 
