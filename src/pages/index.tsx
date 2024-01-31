51cbfd2cbfea48209af318095441ecbfca34a435
Zrossiz <soon.pluxury@mail.ru>
2024-01-31 16:57:11 +0300

    feat: added why us section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -54,7 +54,7 @@ const Home = () => {
       <section className={styles.slider}>
         <Slider title="Новинки" />
       </section>
-      <section className={styles.firstAdvantages}>
+      <section className={styles.advantages}>
         <div className={styles.imgWrapper}>
           <Image fill src={'/mgu.png'} alt="Индивидуальный подход" />
         </div>
@@ -107,6 +107,38 @@ const Home = () => {
       </section>
       <section className={styles.slider}>
         <Slider title="Лучшие предложения" />
+      </section>
+      <section className={styles.whyChoose}>
+        <div className={styles.titleWrapper}>
+          <HTag tag="h2">
+            Почему стоить выбирать <br /> Пушишки?
+          </HTag>
+        </div>
+        <div className={styles.advantagesWrapper}>
+          <ul>
+            <li className={styles.advantage}>
+              <div className={styles.imgWrapper}>
+                <Image src={'/icons/Delivery.svg'} width={50} height={50} alt="Быстрая доставка" />
+              </div>
+              <HTag tag="h3">Быстрая доставка</HTag>
+              <span>Доставим вам товар в <br /> ближайшее время после заказа</span>
+            </li>
+            <li className={styles.advantage}>
+              <div className={styles.imgWrapper}>
+                <Image src={'/icons/Delivery.svg'} width={50} height={50} alt="Быстрая доставка" />
+              </div>
+              <HTag tag="h3">Быстрая доставка</HTag>
+              <span>Доставим вам товар в <br /> ближайшее время после заказа</span>
+            </li>
+            <li className={styles.advantage}>
+              <div className={styles.imgWrapper}>
+                <Image src={'/icons/Delivery.svg'} width={50} height={50} alt="Быстрая доставка" />
+              </div>
+              <HTag tag="h3">Быстрая доставка</HTag>
+              <span>Доставим вам товар в <br /> ближайшее время после заказа</span>
+            </li>
+          </ul>
+        </div>
       </section>
     </>
   );
