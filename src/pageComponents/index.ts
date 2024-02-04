6df3010c6fc381efc6057a85ca1cc88d091d349e
Zrossiz <soon.pluxury@mail.ru>
2024-02-05 00:51:34 +0300

    refactor: quiz removed to page components

--- src/pageComponents/index.ts
+++ src/pageComponents/index.ts
@@ -1,2 +1,3 @@
 export * from './Slider/Slider';
 export * from './Questions/Questions';
+export * from './Quiz/Quiz';
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,15 +6,9 @@ import { HTag } from "@/elements";
 import cn from 'classnames';
 import { inView, motion, useInView } from "framer-motion";
 import { useRef } from "react";
-import { Questions, Slider } from "@/pageComponents";
+import { Questions, Quiz, Slider } from "@/pageComponents";
 
 const Home = () => {
-  const ref = useRef(null)
-  const isInView = useInView(ref);
-  const ref2 = useRef(null)
-  const isInView2 = useInView(ref);
-  const ref3 = useRef(null)
-  const isInView3 = useInView(ref);
   return (
     <>
       <section className={styles.firstDisplay}>
@@ -148,56 +142,7 @@ const Home = () => {
           </ul>
         </div>
       </section>
-      <section className={styles.quiz}>
-        <div className={styles.countWrapper}>
-          <span>( Всего 3 вопроса )</span>
-        </div>
-        <div className={styles.titleWrapper}>
-          <motion.span      
-            ref={ref}
-            transition={{ delay: 0.6, duration: 0.4 }}
-            initial={{ y: 30, opacity: 0 }}
-            animate={{ y: isInView ? 0 : 30, opacity: isInView ? 1 : 0 }}
-          >
-            Подобрать
-          </motion.span>
-          <div className={styles.secondLineWrapper}>
-            <motion.span      
-              ref={ref2}
-              initial={{ y: 30, opacity: 0 }}
-              animate={{ y: isInView2 ? 0 : 30, opacity: isInView2 ? 1 : 0 }}
-              transition={{ delay: 0.8, duration: 0.4 }}
-            >
-              подарок
-            </motion.span>
-            <div className={styles.buttonWrapper}>
-              <button>
-                <Image src={'/icons/SliderNavArrowGreen.svg'} width={46} height={46} alt="Открыть" />
-                <Image src={'/icons/SliderNavArrow.svg'} width={46} height={46} alt="Открыть" />
-              </button>
-            </div>
-            <motion.span      
-              ref={ref3}
-              transition={{ delay: 1, duration: 0.4 }}
-              initial={{ y: 30, opacity: 0 }}
-              animate={{ y: isInView3 ? 0 : 30, opacity: isInView3 ? 1 : 0 }}
-            >
-              ребенку
-            </motion.span>
-          </div>
-        </div>
-        <div className={styles.descWrapper}>
-          <span className={styles.darkFont}>Не требуем номер телефона!</span>
-          <br />
-          <span className={styles.lightFont}>
-            Вы получите только
-            <br /> 
-            список подходящих
-            <br />
-            электромобилей
-          </span>
-        </div>
-      </section>
+      <Quiz />
       <section className={styles.question}>
         <div className={styles.titleWrapper}>
           <HTag tag="h2">
