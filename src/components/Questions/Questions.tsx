5e9718524c1af1871e809d5ee6d3bd32932a4e22
Zrossiz <soon.pluxury@mail.ru>
2024-02-04 09:46:07 +0300

    feat: added popular question

--- src/components/Questions/Questions.tsx
+++ src/components/Questions/Questions.tsx
@@ -1,38 +1,94 @@
 import cn from 'classnames';
+import Image from 'next/image';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import styles from './Questions.module.scss';
 import { useState } from "react";
+import { HTag } from '@/elements';
+import { motion, AnimatePresence } from "framer-motion";
+import { themes } from './Data';
 
-enum EState {
-    Delivery,
-    Assembly,
-    Payment
-}
 
 export const Questions = () => {
-    const [menu, setMenu] = useState<EState>(EState.Delivery)
+    const [menu, setMenu] = useState<number>(0);
     return (
         <>
             <div className={styles.paginationWrapper}>
                 <ul>
-                    <li className={cn({
-                        [styles.active]: menu === EState.Delivery
-                    })} onClick={() => setMenu(EState.Delivery)}>Доставка</li>
-                    <li className={cn({
-                        [styles.active]: menu === EState.Assembly
-                    })} onClick={() => setMenu(EState.Assembly)}>Сборка</li>
-                    <li className={cn({
-                        [styles.active]: menu === EState.Payment
-                    })} onClick={() => setMenu(EState.Payment)}>Оплата</li>
+                    {themes.map((item, index:number) => {
+                        return (
+                            <li 
+                                className={cn({
+                                    [styles.green]: menu === index
+                                })}
+                                onClick={() => setMenu(index)}
+                                key={index}
+                            >
+                                {item.title}
+                                {index === menu ? (
+                                    <motion.div className={styles.active} layoutId="underline" />
+                            ) : null}
+                            </li>
+                        )
+                    })}
                 </ul>
             </div>
             <div className={styles.questionsWrapper}>
-                <ul>
-                    <li>1</li>
-                    <li>2</li>
-                    <li>3</li>
-                </ul>
+                <div className={styles.questions}>
+                    <ul>
+                        {
+                            themes[menu].questions.map((item, index) => {
+                                const [activeQuestion, setActiveQuestion] = useState<boolean>(false);
+                                return (
+                                    <li className={styles.question} key={index}>
+                                    <AnimatePresence>
+                                        <motion.div
+                                            className={styles.titleWrapper} 
+                                            onClick={() => setActiveQuestion(!activeQuestion)}
+                                        >
+                                            <HTag tag='h3'>{item.question}</HTag>
+                                            <div className={styles.iconWrapper}>
+                                                <Image src={'/icons/plus.svg'} height={26} width={26} alt='Открыть'/>
+                                            </div>
+                                        </motion.div>
+                                        {
+                                            activeQuestion && (
+                                            <motion.div 
+                                                className={cn(styles.answerWrapper, {
+                                                    [styles.active]: activeQuestion
+                                                })}
+                                                initial={{ y: 10, opacity: 0 }}
+                                                animate={{ y: 0, opacity: 1 }}
+                                                exit={{ y: -10, opacity: 0 }}
+                                                transition={{ duration: 0.4 }}
+                                            >
+                                                <span>
+                                                {item.answer}
+                                                </span>
+                                            </motion.div>
+                                            )
+                                        }
+                                    </AnimatePresence>
+                                </li>
+                                )
+                            })
+                        }
+                    </ul>
+                </div>
+                <div className={styles.popularQuestionWrapper}>
+                    <div className={styles.popularQuestion}>
+                        <div className={styles.questionWrapper}>
+                            <HTag tag="h3">
+                                {themes[menu].popular?.title}
+                            </HTag>
+                        </div>
+                        <div className={styles.answerWrapper}>
+                            <span>
+                                {themes[menu].popular?.answer}
+                            </span>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
