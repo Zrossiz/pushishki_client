242dc23f3c12349b693df3d8a4a65b89a85335ac
Zrossiz <soon.pluxury@mail.ru>
2024-02-07 21:59:46 +0300

    feat: added dynamic breadcrumbs

--- src/components/Breadcrumbs/Breadcrumbs.tsx
+++ src/components/Breadcrumbs/Breadcrumbs.tsx
@@ -1,15 +1,19 @@
 import Link from 'next/link';
 import styles from './Breadcrumbs.module.scss';
 import { BreadcrumbsProps } from './Breadcrumbs.props';
+import cn from 'classnames';
 
 export const Breadcrumbs = ({breadcrumbs}: BreadcrumbsProps) => {
     return (
-        <ul>
+        <ul className={styles.wrapper}>
             {
                 breadcrumbs.map((item, index) => {
                     return (
-                        <li className={styles.linkWrapper} key={index}>
+                        <li className={cn(styles.linkWrapper)} key={index}>
                             <Link className={styles.link} href={item.path}>{item.name}</Link>
+                            {breadcrumbs.length !== index + 1 && (
+                                <div className={styles.dot}></div>
+                            )}
                         </li>
                     )
                 })
