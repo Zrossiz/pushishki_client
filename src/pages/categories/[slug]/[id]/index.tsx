1ab45fa84e15df300ce88b71a1289d6fddc0b654
Zrossiz <soon.pluxury@mail.ru>
2024-02-19 19:35:28 +0300

    feat: added breadcrumbs for card item page

--- src/pages/categories/[slug]/[id]/index.tsx
+++ src/pages/categories/[slug]/[id]/index.tsx
@@ -2,10 +2,40 @@ import { getAccessories, getBestsellers } from "@/api";
 import { withLayout } from "@/layout/Layout";
 import { Form, Questions, Slider } from "@/pageComponents";
 import { IProductCardPageProps } from "@/types";
+import styles from '../../../../styles/Card.module.scss';
+import { Breadcrumbs } from "@/components";
+import { useRouter } from "next/router";
+import { HTag } from "@/elements";
 
 const ProductCardPage = ({ bestSellers, acessories }: IProductCardPageProps) => {
+    const router = useRouter();
+    console.log(router);
     return (
-        <>
+        <>  
+            <div className={styles.itemDescriptionWrapper}>
+                <div className={styles.galleryAndDescriptionWrapper}>1</div>
+                <div className={styles.aboutItemWrapper}>
+                    <div className={styles.breadcrumbsWrapper}>
+                        <Breadcrumbs breadcrumbs={[
+                            {
+                                name: 'Главная',
+                                path: '/'
+                            },
+                            {
+                                name: 'Категории',
+                                path: '/categories'
+                            },
+                            {
+                                name: 'Каталог',
+                                path: `/categories/${router.query.slug}`
+                            }
+                        ]}/>
+                    </div>
+                    <div className={styles.titleWrapper}>
+                        <HTag tag="h1">Электромобиль</HTag>
+                    </div>
+                </div>
+            </div>
             <Slider title="Аксессуары" products={acessories?.data} />
             <Slider title="Лучшие предложения" products={bestSellers} />
             <Questions />
