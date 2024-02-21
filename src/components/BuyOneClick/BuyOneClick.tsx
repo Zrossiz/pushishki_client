59ac63dad68cfdf19d380808c72b22b7f1963ee5
Zrossiz <soon.pluxury@mail.ru>
2024-02-21 13:44:36 +0300

    feat: added animations for buy on click form

--- src/components/BuyOneClick/BuyOneClick.tsx
+++ src/components/BuyOneClick/BuyOneClick.tsx
@@ -2,19 +2,27 @@ import { HTag } from '@/elements';
 import styles from './BuyOneClick.module.scss';
 import { BuyOneClickProps } from './BuyOneClick.props';
 import Image from 'next/image';
+import { AnimatePresence, motion } from 'framer-motion';
 
 
 export const BuyOneClick = ({ setOpen }: BuyOneClickProps) => {
     return (
         <div className={styles.wrapper}>
-            <div className={styles.formWrapper}>
-                <div className={styles.close} onClick={() => setOpen(false)}>
-                    <Image src="/icons/Close.svg" width={30} height={30} alt="Закрыть" />
-                </div>
-                <div>
-                    <HTag tag='h2'>Купить в один клик</HTag>
-                </div>
-            </div>
+            <AnimatePresence>
+                <motion.div
+                    className={styles.formWrapper}
+                    initial={{ y: -10, opacity: 0 }}
+                    animate={{ y: 0, opacity: 1 }}
+                    transition={{ duration: 0.6, }}
+                >
+                    <div className={styles.close} onClick={() => setOpen(false)}>
+                        <Image src="/icons/Close.svg" width={30} height={30} alt="Закрыть" />
+                    </div>
+                    <div>
+                        <HTag tag='h2'>Купить в один клик</HTag>
+                    </div>
+                </motion.div>
+            </AnimatePresence>
         </div>
     )
 }
