46771914786bd28cb985ec34eb30f53fc78f08ad
Zrossiz <soon.pluxury@mail.ru>
2024-02-28 09:43:22 +0300

    feat: added rating component boilerplate

---
+++ src/components/Rating/Rating.props.ts
@@ -0,0 +1,4 @@
+export interface RatingProps {
+    rating: number;
+    setRating: (arg0: number) => void;
+}
---
+++ src/components/Rating/Rating.tsx
@@ -0,0 +1,8 @@
+import styles from './Rating.module.scss';
+import { RatingProps } from './Rating.props';
+
+export const Rating = ({ rating, setRating }: RatingProps) => {
+    return (
+        <div>rating</div>
+    )
+}
