0716e5610f808adfc5c644c88f18767c93ba76bb
Zrossiz <soon.pluxury@mail.ru>
2024-03-01 16:12:28 +0300

    chore: added content for why us section

--- src/pageComponents/WhyChoose/WhyChoose.tsx
+++ src/pageComponents/WhyChoose/WhyChoose.tsx
@@ -21,17 +21,21 @@ export const WhyChoose = () => {
             </li>
             <li className={styles.advantage}>
               <div className={styles.imgWrapper}>
-                <Image src={'/icons/Delivery.svg'} width={50} height={50} alt="Быстрая доставка" />
+                <Image src={'/icons/Help.svg'} width={50} height={50} alt="Помощь в выборе" />
               </div>
-              <HTag tag="h3">Быстрая доставка</HTag>
-              <span>Доставим вам товар в <br /> ближайшее время после заказа</span>
+              <HTag tag="h3">Помощь в выборе</HTag>
+              <span>
+                Не знаете какая модель вам подойдет?<br />
+                Мы всегда поможем выбрать то, 
+                что нужно вам и вашему ребенку
+              </span>
             </li>
             <li className={styles.advantage}>
               <div className={styles.imgWrapper}>
-                <Image src={'/icons/Delivery.svg'} width={50} height={50} alt="Быстрая доставка" />
+                <Image src={'/icons/Delivery.svg'} width={50} height={50} alt="Лучшие предложения" />
               </div>
-              <HTag tag="h3">Быстрая доставка</HTag>
-              <span>Доставим вам товар в <br /> ближайшее время после заказа</span>
+              <HTag tag="h3">Лучшие предложения</HTag>
+              <span>В нашем каталоге вы точно найти что-то уникальное и неповторимое для своего ребенка</span>
             </li>
           </ul>
         </div>
