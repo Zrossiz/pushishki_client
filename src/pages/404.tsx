e79cf0f54386a6711b938febf30a4f7741b27e08
Zrossiz <soon.pluxury@mail.ru>
2024-03-01 10:58:44 +0300

    feat: added styles for 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,21 @@
 import { withLayout } from "@/layout/Layout";
+import styles from '../styles/404.module.scss'; 
+import { LinkButton } from "@/elements";
+import Link from "next/link";
 
 const Error404 = () => {
     return (
-        <div>404</div>
+        <section className={styles.wrapper}>
+            <h1>Ошибка <span>404</span></h1>
+            <div className={styles.desc}>
+                неправильно набран адрес, или такой <br />
+                страницы на сайте больше не существует  
+            </div>
+            <div className={styles.linksWrapper}>
+                <LinkButton element="link" href="/">На главную</LinkButton>
+                <Link className={styles.categoriesLink} href="/categories">В&nbsp;категории</Link>
+            </div>
+        </section>
     )
 };
 
