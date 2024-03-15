b0fd7952e0f3bc78dfdcfc5b487f2871ae59a164
Zrossiz <soon.pluxury@mail.ru>
2024-03-15 21:11:28 +0300

    chore: added styles for login page

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,21 +1,36 @@
 import { HTag, Input, LinkButton } from '@/elements';
 import styles from '../../styles/Login.module.scss';
 import { useState } from 'react';
+import Head from 'next/head';
 
 const LoginPage = () => {
     const [name, setName] = useState<string>('');
     const [password, setPassword] = useState<string>('');
+
+    const login = async () => {
+        try {
+            
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
     return (
-        <div className={styles.loginPageWrapper}>
-            <div className={styles.formWrapper}>
-                <HTag tag='h2'>Введите логин и пароль</HTag>
-                <Input type={'text'} value={name} onChange={setName} placeholder='Имя' />
-                <Input type={'text'} value={password} onChange={setPassword} placeholder='Пароль' />
-                <LinkButton element={'button'}>
-                    Войти
-                </LinkButton>
+        <>
+            <Head>
+                <title>Логин | Пушишки</title>
+            </Head>
+            <div className={styles.loginPageWrapper}>
+                <div className={styles.formWrapper}>
+                    <HTag tag='h2' color='white'>Введите логин и пароль</HTag>
+                    <Input type={'text'} value={name} onChange={setName} placeholder='Имя' />
+                    <Input type={'text'} value={password} onChange={setPassword} placeholder='Пароль' />
+                    <LinkButton element={'button'} onClick={login}>
+                        Войти
+                    </LinkButton>
+                </div>
             </div>
-        </div>
+        </>
     )
 };
 
