a4e3d645a84f1ae829a82ab83f73f220c5cb6a6b
Zrossiz <soon.pluxury@mail.ru>
2024-03-18 20:26:22 +0300

    chore: added basket page

---
+++ src/pages/admin/basket.tsx
@@ -0,0 +1,11 @@
+import { AdminLayout } from "@/layout/adminLayout/AdminLayout"
+
+const BasketPage = () => {
+    return (
+        <AdminLayout>
+            <div>Страница корзины</div>
+        </AdminLayout>
+    );
+};
+
+export default BasketPage;
