376b0049e433d7087704f41da22bab23a00d3610
Zrossiz <soon.pluxury@mail.ru>
2024-03-18 20:24:36 +0300

    chore: added color page

---
+++ src/pages/admin/color.tsx
@@ -0,0 +1,11 @@
+import { AdminLayout } from "@/layout/adminLayout/AdminLayout"
+
+const ColorPage = () => {
+    return (
+        <AdminLayout>
+            <div>Цвета</div>
+        </AdminLayout>
+    );
+};
+
+export default ColorPage;
