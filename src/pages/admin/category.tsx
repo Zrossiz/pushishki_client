df4c67e03676e8841e0ffd10aa6d63801916ccb9
Zrossiz <soon.pluxury@mail.ru>
2024-03-18 20:21:51 +0300

    chore: added category page

---
+++ src/pages/admin/category.tsx
@@ -0,0 +1,11 @@
+import { AdminLayout } from "@/layout/adminLayout/AdminLayout";
+
+const CategoryPage = () => {
+    return (
+        <AdminLayout>
+            <div>Категории</div>
+        </AdminLayout>
+    );
+};
+
+export default CategoryPage;
