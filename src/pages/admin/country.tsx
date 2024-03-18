30fe5aca192b271dc3b326951e3ec9a46f3ca5df
Zrossiz <soon.pluxury@mail.ru>
2024-03-18 19:51:50 +0300

    chore: added country page

---
+++ src/pages/admin/country.tsx
@@ -0,0 +1,11 @@
+import { AdminLayout } from "@/layout/adminLayout/AdminLayout"
+
+const CountryPage = () => {
+    return (
+        <AdminLayout>
+            <div>Страна</div>
+        </AdminLayout>
+    )
+};
+
+export default CountryPage;
