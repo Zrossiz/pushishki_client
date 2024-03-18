1860febf52933a64d6252582d3f7ac4219e8e2cf
Zrossiz <soon.pluxury@mail.ru>
2024-03-18 20:20:34 +0300

    chore: added order page

---
+++ src/pages/admin/order.tsx
@@ -0,0 +1,11 @@
+import { AdminLayout } from "@/layout/adminLayout/AdminLayout"
+
+const OrderPage = () => {
+    return (
+        <AdminLayout>
+            <div>Order</div>
+        </AdminLayout>
+    );
+};
+
+export default OrderPage;
