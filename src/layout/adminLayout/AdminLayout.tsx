0e4e541d1d05b2c953e0996b862c4916bdc083c0
Zrossiz <soon.pluxury@mail.ru>
2024-03-18 19:33:02 +0300

    refactor: added admin layout

--- src/layout/adminLayout/AdminLayout.tsx
+++ src/layout/adminLayout/AdminLayout.tsx
@@ -4,17 +4,11 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import Head from 'next/head';
 import cn from 'classnames';
-import { axiosInst } from '@/utils';
-import getConfig from 'next/config';
-import { AdminLayout } from '@/layout/adminLayout/AdminLayout';
+import { AdminLayoutProps } from './AdminLayout.prop';
 
-const { publicRuntimeConfig } = getConfig();
-const { API_URL } = publicRuntimeConfig;
-
-const AdminPage = () => {
+export const AdminLayout = ({ children }: AdminLayoutProps) => {
     const router = useRouter();
     const [active, setActive] = useState<string>('Товар');
-    const [items, setItems] = useState<any[]>([]);
 
     const entities: {name: string, slug: string}[] = [
         {
@@ -61,10 +55,33 @@ const AdminPage = () => {
     };
 
     return (
-        <AdminLayout>
-            <div>Инфо</div>
-        </AdminLayout>
+        <>
+            <Head>
+                <title>Администрирование</title>
+            </Head>
+            <div className={styles.adminWrapper}>
+                <div className={styles.sidebarWrapper}>
+                    <div className={styles.active}>
+                        {active}
+                    </div>
+                    <ul className={styles.list}>
+                        {entities.map((item, index) => {
+                            return (
+                                <li 
+                                    className={cn(styles.item, {
+                                        [styles.active]: item.name === active
+                                    })} 
+                                    key={index} 
+                                    onClick={() => switchEntity(item)}
+                                >{item.name}</li>
+                            )
+                        })}
+                    </ul>
+                </div>
+                <div className={styles.infoWrapper}>
+                        {children}
+                </div>
+            </div>
+        </>
     );
 };
-
-export default AdminPage;
