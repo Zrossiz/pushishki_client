a5089c37e1409ed9861cbf6cb5cb2718337434fe
Zrossiz <soon.pluxury@mail.ru>
2024-03-18 19:35:53 +0300

    chore: added admin product page

---
+++ src/pages/admin/product.tsx
@@ -0,0 +1,11 @@
+import { AdminLayout } from "@/layout/adminLayout/AdminLayout"
+
+const ProductPage = () => {
+    return (
+        <AdminLayout>
+            <div>товар</div>
+        </AdminLayout>
+    );
+};
+
+export default ProductPage;
