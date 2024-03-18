acc5248581c6774ac64b04e83dfda7734674c8a1
Zrossiz <soon.pluxury@mail.ru>
2024-03-18 20:23:38 +0300

    chore: added brand page

---
+++ src/pages/admin/brand.tsx
@@ -0,0 +1,11 @@
+import { AdminLayout } from "@/layout/adminLayout/AdminLayout"
+
+const BrandPage = () => {
+    return (
+        <AdminLayout>
+            <div>Бренд</div>
+        </AdminLayout>
+    );
+};
+
+export default BrandPage;
