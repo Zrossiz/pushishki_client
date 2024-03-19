b3950127e8e22aad8e72056dcb657553a23acc83
Zrossiz <soon.pluxury@mail.ru>
2024-03-19 11:00:25 +0300

    feat: added styles for list item

--- src/pages/admin/product.tsx
+++ src/pages/admin/product.tsx
@@ -1,27 +1,56 @@
 import { getAllProducts } from '@/api';
 import { AdminLayout } from '@/layout/admin/AdminLayout';
-import { IProductWithLength } from '@/types';
+import { IProduct, IProductWithLength } from '@/types';
 import { useEffect, useState } from 'react';
 import styles from '../../styles/admin/Product.module.scss';
+import getConfig from 'next/config';
+import Image from 'next/image';
+
+const { publicRuntimeConfig } = getConfig();
+const { FILESERVER_URL } = publicRuntimeConfig;
 
 const ProductPage = () => {
-  let products: IProductWithLength | { message: string };
+  const [products, setProducts] = useState<IProductWithLength | { message: string }>();
   const [page, setPage] = useState<number>(1);
 
   useEffect(() => {
-    (async () => {
-      products = await getAllProducts(page);
-    })();
-  }, []);
-  
+    const fetchProducts = async () => {
+      try {
+        const productsData = await getAllProducts(page);
+        setProducts(productsData);
+      } catch (error) {
+        setProducts({ message: 'Error fetching products' });
+      }
+    };
+
+    fetchProducts();
+  }, [page]);
 
   return (
     <AdminLayout>
-      <div className={styles.addButtonWrapper}>
-        <button>
-          Добавить товар
-        </button>
-      </div>
+      <>
+        <div className={styles.addButtonWrapper}>
+          <button>
+            Добавить товар
+          </button>
+        </div>
+        <div className={styles.listWrapper}>
+          {products && 'data' in products && products.data.map((item: IProduct, index: number) => (
+            <div className={styles.itemWrapper} key={index}>
+              <div className={styles.image}>
+                <Image fill alt={item.name} src={`${FILESERVER_URL}/upload/${item.image}`} />
+              </div>
+              <div className={styles.name}>
+                {item.name}
+              </div>
+              <div className={styles.options}>
+                <div className={styles.edit}>Редактировать</div>
+                <div className={styles.delete}>Удалить</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </>
     </AdminLayout>
   );
 };
