9084239b4187939246f56304d119cf99f165aced
Zrossiz <soon.pluxury@mail.ru>
2024-04-01 11:09:32 +0300

    feat: added admin color page structure

--- src/pages/admin/color.tsx
+++ src/pages/admin/color.tsx
@@ -1,9 +1,30 @@
 import { AdminLayout } from '@/layout/admin/AdminLayout';
+import styles from '../../styles/admin/Color.module.scss';
+import { LinkButton } from '@/elements';
+import { useState } from 'react';
+import { IColor } from '@/types/Color';
+import { color } from 'framer-motion';
 
 const ColorPage = () => {
+  const [create, setCreate] = useState<boolean>(false);
+  const [colors, setColors] = useState<IColor[]>([]);
+
   return (
     <AdminLayout>
-      <div>Цвета</div>
+      <>
+        <div className={styles.addButtonWrapper}>
+          <LinkButton element="button" onClick={() => setCreate(true)}>
+            Добавить
+          </LinkButton>
+        </div>
+        <div className={styles.listWrapper}>
+          {colors.map((item) => {
+            return (
+              <div>{item.color}</div>
+            )
+          })}
+        </div>
+      </>
     </AdminLayout>
   );
 };
