a694409b9a378934b4769a9275cc423bcf2f5432
Zrossiz <soon.pluxury@mail.ru>
2024-04-23 10:43:06 +0300

    feat: added delete file api

--- src/api/Files.ts
+++ src/api/Files.ts
@@ -28,3 +28,17 @@ export const uploadFiles = async (files: File[] | File): Promise<{ message: stri
     };
   }
 };
+
+export const deleteFile = async (fileName: string): Promise<{ message: string }> => {
+  try {
+    await axios.delete(`${FILESERVER_URL}/upload/${fileName}`);
+    return {
+      message: 'Успешно'
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      message: 'Ошибка при удалении файла',
+    }
+  };
+};
