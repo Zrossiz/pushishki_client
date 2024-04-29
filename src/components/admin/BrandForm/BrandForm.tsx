156f6c1c7bb19e4e14d872eb27d7b9ca197f12fb
Zrossiz <soon.pluxury@mail.ru>
2024-04-29 15:46:33 +0300

    feat: added update brand func

--- src/components/admin/BrandForm/BrandForm.tsx
+++ src/components/admin/BrandForm/BrandForm.tsx
@@ -2,9 +2,17 @@ import { HTag, Input, LinkButton } from '@/elements';
 import styles from './BrandForm.module.scss';
 import { BrandFormProps } from './BrandForm.props';
 import { useState } from 'react';
+import { updateBrand } from '@/api';
 
 export const BrandForm = ({ setOpen, isEdit, brand }: BrandFormProps) => {
   const [name, setName] = useState<string | undefined>(brand?.name);
+  const updateApiBrand = async () => {
+    if (brand && name) {
+      await updateBrand(brand?.id, name);
+      window.location.reload();
+    };
+  };
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.bg} onClick={() => setOpen(false)}></div>
@@ -14,7 +22,7 @@ export const BrandForm = ({ setOpen, isEdit, brand }: BrandFormProps) => {
           <label>Название бренда</label>
           <Input type={'text'} value={name} onChange={setName} />
         </div>
-        <LinkButton element="button">Обновить</LinkButton>
+        <LinkButton element="button" onClick={() => updateApiBrand()}>Обновить</LinkButton>
       </div>
     </div>
   );
