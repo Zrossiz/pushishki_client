128c8161ad3d76ac6dee3873c12bd1db64cdcecc
Zrossiz <soon.pluxury@mail.ru>
2024-04-29 14:14:27 +0300

    feat: added brand page

--- src/api/Brand.ts
+++ src/api/Brand.ts
@@ -1,9 +1,13 @@
 import { IBrandWithLength } from '@/types';
 import axios from 'axios';
+import getConfig from 'next/config';
+
+const { publicRuntimeConfig } = getConfig();
+const { API_URL } = publicRuntimeConfig;
 
 export const getBrands = async (): Promise<IBrandWithLength | { message: string }> => {
   try {
-    const { data } = await axios.get<IBrandWithLength>(`${process.env.API_URL}/brand`);
+    const { data } = await axios.get<IBrandWithLength>(`${API_URL}/brand`);
 
     return data;
   } catch (err) {
--- src/pages/authorization/admin/brand.tsx
+++ src/pages/authorization/admin/brand.tsx
@@ -1,9 +1,38 @@
 import { AdminLayout } from '@/layout/admin/AdminLayout';
+import styles from '../../../styles/admin/Category.module.scss';
+import { LinkButton } from '@/elements';
+import { useEffect, useState } from 'react';
+import { IBrandWithLength } from '@/types';
+import { getBrands } from '@/api';
+
 
 const BrandPage = () => {
+  const [create, setCreate] = useState<boolean>(false);
+  const [brands, setBrands] = useState<IBrandWithLength>();
+
+  useEffect(() => {
+    (async () => {
+      const apiBrands = await getBrands();
+      if ('data' in apiBrands) {
+        setBrands(apiBrands);
+      }
+    })()
+  }, []);
+
+  console.log(brands);
+
   return (
     <AdminLayout>
-      <div>Бренд</div>
+      <>
+        <div className={styles.addButtonWrapper}>
+          <LinkButton element="button" onClick={() => setCreate(true)}>
+            Добавить
+          </LinkButton>
+        </div>
+        <div className={styles.listWrapper}>
+
+        </div>
+      </>
     </AdminLayout>
   );
 };
