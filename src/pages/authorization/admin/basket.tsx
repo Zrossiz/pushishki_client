da9132d64fd62f1a8f76fec85c4a307d5aa62bee
Zrossiz <soon.pluxury@mail.ru>
2024-04-29 10:43:21 +0300

    feat: added pagination for order page

--- src/pages/authorization/admin/basket.tsx
+++ src/pages/authorization/admin/basket.tsx
@@ -3,31 +3,38 @@ import styles from '../../../styles/admin/Basket.module.scss'
 import { useEffect, useState } from 'react';
 import { IOrder, IOrderWithLength } from '@/types';
 import { getAllOrders } from '@/api';
-import { OrderListItem } from '@/components/admin';
+import { OrderListItem, Pagination } from '@/components/admin';
 
 const BasketPage = () => {
-
+  const [curPage, setCurPage] = useState<number>(1);
   const [orders, setOrders] = useState<IOrderWithLength>();
 
   useEffect(() => {
-    (async() => {
-      const ordersApi = await getAllOrders(1);
+    (async () => {
+      const ordersApi = await getAllOrders(curPage);
       if ('data' in ordersApi) {
         setOrders(ordersApi);
       };
-    })()
-  }, []);
+    })();
+  }, [curPage]);
 
 
   return (
     <AdminLayout>
-      <div className={styles.listWrapper}>
-        {orders?.data.map((item: IOrder) => {
-          return (
-            <OrderListItem order={item} />
-          );
-        })}
-      </div>
+      <>
+        <div className={styles.listWrapper}>
+          {orders?.data.map((item: IOrder) => {
+            return (
+              <OrderListItem order={item} key={item.id} />
+            );
+          })}
+        </div>
+        {orders && orders?.totalPages > 1 &&
+          <div className={styles.paginationWrapper}>
+            <Pagination curPage={curPage} setCurPage={setCurPage} totalPages={orders.totalPages} />
+          </div>
+        }
+      </>
     </AdminLayout>
   );
 };
