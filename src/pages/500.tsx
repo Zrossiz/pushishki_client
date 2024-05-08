9bdf607fd67a65217bd4da732bc58520eaf030be
Zrossiz <soon.pluxury@mail.ru>
2024-05-08 23:02:27 +0300

    chore: added 500 page styles

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -1,13 +1,14 @@
-import { HTag } from "@/elements"
-import { Layout } from "@/layout/client/Layout"
+import { HTag } from "@/elements";
+import { Layout } from "@/layout/client/Layout";
+import styles from '../styles/client/500.module.scss';
 
 const Error500 = () => {
     return (
         <Layout title="Мы скоро все починим!" >
-            <>
+            <div className={styles.wrapper}>
                 <HTag tag="h1">Мы скоро все починим!</HTag>
                 <p>Загляните к нам позже ^_^</p>
-            </>
+            </div>
         </Layout>
     )
 }
