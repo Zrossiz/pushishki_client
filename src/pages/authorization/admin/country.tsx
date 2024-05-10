cc66d338862ebc4dff968614e5efb29b1218cd98
Zrossiz <soon.pluxury@mail.ru>
2024-05-10 12:12:15 +0300

    feat: added admin category page layout

--- src/pages/authorization/admin/country.tsx
+++ src/pages/authorization/admin/country.tsx
@@ -1,9 +1,22 @@
 import { AdminLayout } from '@/layout/admin/AdminLayout';
+import styles from '../../../styles/admin/Country.module.scss';
+import { LinkButton } from '@/elements';
+import { useState } from 'react';
 
 const CountryPage = () => {
+  const [create, setCreate] = useState<boolean>(false);
   return (
     <AdminLayout>
-      <div>Страна</div>
+      <>
+        <div className={styles.addButtonWrapper}>
+          <LinkButton element="button" onClick={() => setCreate(true)}>
+            Добавить
+          </LinkButton>
+          <div className={styles.litsWrapper}>
+            
+          </div>
+        </div>
+      </>
     </AdminLayout>
   );
 };
