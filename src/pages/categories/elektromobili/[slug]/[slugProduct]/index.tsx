ad7719f0d301e34a0431ace2e179db5ca1e4a72e
Zrossiz <soon.pluxury@mail.ru>
2024-07-28 12:40:41 +0300

    chore: added card page component for sub category

--- src/pages/categories/elektromobili/[slug]/[slugProduct]/index.tsx
+++ src/pages/categories/elektromobili/[slug]/[slugProduct]/index.tsx
@@ -1,11 +1,50 @@
-import { Layout } from "@/layout/client/Layout";
-
-const ElektromobiliCardPage = () => {
+import {
+    getAccessories,
+    getBestsellers,
+    getOneProduct,
+    getProductVariants,
+    getReviewsProduct,
+  } from '@/api';
+  import { ProductCard } from '@/pageComponents/ProductCard/ProductCard';
+  import { IProductCardPageProps } from '@/types';
+  
+  const ProductCardPage = ({
+    bestSellers,
+    accessories,
+    product,
+    productVariants,
+    reviews,
+  }: IProductCardPageProps) => {
     return (
-        <Layout>
-            <div>test</div>
-        </Layout>
+      <ProductCard 
+        bestSellers={bestSellers} 
+        accessories={accessories}
+        product={product}
+        productVariants={productVariants}
+        reviews={reviews}
+      />
     )
-}
-
-export default ElektromobiliCardPage;
+  };
+  
+  export default ProductCardPage;
+  
+  export const getServerSideProps = async (context: { params: { slugProduct: string } }) => {
+    const { slugProduct } = context.params;
+  
+    const bestSellers = await getBestsellers();
+    const accessories = await getAccessories();
+    const product = await getOneProduct(slugProduct);
+    const productVariants = await getProductVariants('id' in product ? product.id : 1);
+    const reviews = await getReviewsProduct('id' in product ? product.id : 1, false);
+  
+    return {
+      props: {
+        accessories,
+        bestSellers,
+        product,
+        productVariants,
+        reviews,
+      },
+    };
+  };
+  
