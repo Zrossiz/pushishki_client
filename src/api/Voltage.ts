d2192a1ba1b94f4943d0c503428de989e9e78803
Zrossiz <soon.pluxury@mail.ru>
2024-08-16 15:34:51 +0300

    feat: added create and update voltage queries

--- src/api/Voltage.ts
+++ src/api/Voltage.ts
@@ -1,4 +1,5 @@
 import { IVoltage } from '@/types/Voltage';
+import { axiosInst } from '@/utils';
 import axios from 'axios';
 import getConfig from 'next/config';
 
@@ -15,3 +16,27 @@ export const getAllVoltages = async (): Promise<IVoltage[] | { message: string }
     return { message: 'Ошибка при получении объекта вольтажа' };
   }
 };
+
+export const createVoltage = async (name: number): Promise<IVoltage | { message: string }> => {
+  try {
+    const { data } = await axiosInst.post<IVoltage>(`${API_URL}/voltage`, { name })
+
+    return data;
+  } catch (err) {
+    console.log(err);
+    return { message: 'Ошибка при создании объекта вольтажа' }
+  }
+}
+
+export const updateVoltage = async (
+  id: number, name: number
+): Promise<IVoltage | { message: string }> => {
+  try {
+    const { data } = await axiosInst.post<IVoltage>(`${API_URL}/voltage/${id}`, { name })
+
+    return data;
+  } catch (err) {
+    console.log(err);
+    return { message: 'Ошибка при обновлении объекта вольтажа' }
+  }
+}
