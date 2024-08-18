7498c35b48f00a88efdd2d8a79a98c18bcd7906c
Zrossiz <soon.pluxury@mail.ru>
2024-08-18 14:42:26 +0300

    feat: added create and update drive queries

--- src/api/Drive.ts
+++ src/api/Drive.ts
@@ -1,4 +1,5 @@
 import { IDrive } from '@/types/Drive';
+import { axiosInst } from '@/utils';
 import axios from 'axios';
 import getConfig from 'next/config';
 
@@ -13,7 +14,33 @@ export const getAllDrives = async (): Promise<IDrive | { message: string }> => {
   } catch (err) {
     console.log(err);
     return {
-      message: 'Ошибка при создании заказа',
+      message: 'Ошибка при получении приводов',
     };
   }
 };
+
+export const createDrive = async (name: string): Promise<IDrive | { message: string }> => {
+  try {
+    const { data } = await axiosInst.post<IDrive>(`${API_URL}/drive`, { name });
+
+    return data;
+  } catch (err) {
+    console.log(err);
+    return {
+      message: 'Ошибка при создании привода'
+    }
+  }
+}
+
+export const updateDrive = async (id: number, name: string): Promise<IDrive | { message: string }> => {
+  try {
+    const { data } = await axiosInst.post<IDrive>(`${API_URL}/drive/${id}`, { name });
+
+    return data;
+  } catch (err) {
+    console.log(err);
+    return {
+      message: 'Ошибка при обновлении привода'
+    }
+  }
+}
