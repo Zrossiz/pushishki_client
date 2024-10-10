aed709b9cee1674530ec2b8ec804edd467d3acd1
Zrossiz <soon.pluxury@mail.ru>
2024-10-10 13:31:19 +0300

    feat: created manufacturer admin page

--- src/pages/authorization/admin/manufacturer.tsx
+++ src/pages/authorization/admin/manufacturer.tsx
@@ -1,11 +1,37 @@
+import { getAllManufacturers } from "@/api";
+import { HTag, LinkButton } from "@/elements";
 import { AdminLayout } from "@/layout/admin/AdminLayout"
+import { IManufacturer } from "@/types/Manufacturer";
+import { useEffect, useState } from "react";
+import styles from '../../../styles/admin/Default.module.scss';
 
 const ManufacturerPage = () => {
+    const [manufacturers, setManufacturers] = useState<IManufacturer[]>([]);
+
+    useEffect(() => {
+        (async () => {
+            const apiManufacturers = await getAllManufacturers();
+            if (Array.isArray(apiManufacturers)) {
+                setManufacturers(apiManufacturers);
+            }
+        })
+    }, []);
+
     return (
         <AdminLayout>
             <>
-                <div>manufacturer page</div>
-            
+                <div className={styles.addButtonWrapper}>
+                    <LinkButton element="button">
+                        Добавить
+                    </LinkButton>
+                </div>
+                <div className={styles.listWrapper}>
+                    {manufacturers.length > 0 ? (
+                        <div>Элементы</div>
+                    ) : (
+                        <HTag tag="h3">Ничего не найдено</HTag>
+                    )}
+                </div>
             </>
         </AdminLayout>
     )
