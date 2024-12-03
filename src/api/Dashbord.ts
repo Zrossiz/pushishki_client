301827eac3ede3d7a20db89c24e0d2525c436bde
Zrossiz <soon.pluxury@mail.ru>
2024-12-04 02:24:07 +0300

    chore: added get most sellings products api

--- src/api/Dashbord.ts
+++ src/api/Dashbord.ts
@@ -1,4 +1,5 @@
-import { axiosInst, fromDateToString } from "@/utils"
+import { IProduct } from "@/types";
+import { axiosInst, getBoundaryDatesByduration } from "@/utils"
 import getConfig from 'next/config';
 
 const { publicRuntimeConfig } = getConfig();
@@ -6,28 +7,7 @@ const { API_URL } = publicRuntimeConfig;
 
 export const getAveragePrice = async (duration: string): Promise<{price: number}> => {
     try {
-        let dateFrom: Date = new Date();
-        const dateTo: Date = new Date();
-
-        switch (duration) {
-            case 'day':
-                dateFrom.setDate(dateFrom.getDate() - 1)
-                break;
-            case 'week':
-                dateFrom.setDate(dateFrom.getDate() - 7)
-                break;
-            case 'month':
-                dateFrom.setMonth(dateFrom.getMonth() - 1)
-                break
-            case 'year':
-                dateFrom.setFullYear(dateFrom.getFullYear() - 1)
-                break
-            default:
-                return {price: 0}
-        }
-
-        const fromDateString = fromDateToString(dateFrom);
-        const toDateString = fromDateToString(dateTo)
+        const [fromDateString, toDateString] = getBoundaryDatesByduration(duration);
 
         const { data } = await axiosInst.get<{price: number}>(`
             ${API_URL}/dashbord/average-sum?dayFrom=${fromDateString}&dayTo=${toDateString}
@@ -38,5 +18,19 @@ export const getAveragePrice = async (duration: string): Promise<{price: number}
         return {
             price: 0
         }
+    }
+}
+
+export const getMostSellingProducts = async (duration: string): Promise<IProduct[]> => {
+    try {
+        const [fromDateString, toDateString] = getBoundaryDatesByduration(duration);
+
+        const { data } = await axiosInst.get<IProduct[]>(`
+            ${API_URL}/dashbord/most-selling?dayFrom=${fromDateString}&dayTo=${toDateString}
+        `);
+
+        return data;
+    } catch (err) {
+        return [];
     }
 }
--- src/utils/dateFormat.ts
+++ src/utils/dateFormat.ts
@@ -2,3 +2,30 @@ export const fromDateToString = (date: Date): string => {
     const res = date.toISOString().split('T')[0];
     return res;
 } 
+
+export const getBoundaryDatesByduration = (duration: string): string[] => {
+    let dateFrom: Date = new Date();
+    const dateTo: Date = new Date();
+
+    switch (duration) {
+        case 'day':
+            dateFrom.setDate(dateFrom.getDate() - 1)
+            break;
+        case 'week':
+            dateFrom.setDate(dateFrom.getDate() - 7)
+            break;
+        case 'month':
+            dateFrom.setMonth(dateFrom.getMonth() - 1)
+            break
+        case 'year':
+            dateFrom.setFullYear(dateFrom.getFullYear() - 1)
+            break
+        default:
+            return ["", ""]
+    }
+
+    const fromDateString = fromDateToString(dateFrom);
+    const toDateString = fromDateToString(dateTo);
+
+    return [fromDateString, toDateString]
+}
